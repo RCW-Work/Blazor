24ac4f1c31db4203552fde6ac32d4de8eaeca80d
Richard-RCW <rcw.work@hotmail.com>
2020-10-21 18:36:21 +0800

    20201021 練習寫ModbusTCP on Blazor 還有很多But 不要上傳喔

---
+++ BlazorSCADA/BlazorSCADA/ModbusTCP/ModbusServer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace BlazorSCADA.ModbusTCP
+{
+    public class ModbusServer
+    {
+
+    };
+    public class ModbusMaster
+    {
+        public string slaveIP { get; set; }
+        public ushort slavePort { get; set; }
+
+        public bool isConnect = false;
+        public ModbusTCP.ModbusServerService MBmaster { get; set; }
+
+
+    }
+    public class RcvData
+    { 
+    
+    }
+}
