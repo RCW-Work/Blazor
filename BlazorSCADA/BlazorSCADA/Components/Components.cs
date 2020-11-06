fef12e2be34ba4379faabe00a451c09b77246ce3
Richard-RCW <rcw.work@hotmail.com>
2020-11-06 18:34:48 +0800

    Practice

--- BlazorSCADA/BlazorSCADA/Components/Components.cs
+++ BlazorSCADA/BlazorSCADA/Components/Components.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
@@ -12,6 +13,21 @@ namespace BlazorSCADA.Components
         public class testBtn
         {
             public delegate void testDelegate();
-        }
+            public event testDelegate onTrigger;
+
+            public void testDelegateFunction() { }
+
+            public void testLoop()
+            {
+                testDelegate cbfunction = new testDelegate(testDelegateFunction);
+
+                for (int i = 0; i < 10; i++)
+                {
+                    cbfunction.Invoke();
+                }
+            }
+            
+
+    }
     }
 }
